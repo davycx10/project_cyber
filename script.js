0d34304b3d599acec4464bf5aeab56973bb17443
davycx <dave.israel785@gmail.com>
2025-02-10 14:59:55 +0100

    add cookie banner

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.querySelector(".hamburger").click(); // Simule un clic sur le bouton
+
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function acceptCookies() {
+    setCookie("cookiesAccepted", "true", 30);
+    document.getElementById("cookieBanner").style.display = "none";
+}
+
+function declineCookies() {
+    setCookie("cookiesAccepted", "false", 30);
+    document.getElementById("cookieBanner").style.display = "none";
+}
+
+window.onload = function() {
+    if (getCookie("cookiesAccepted") === null) {
+        document.getElementById("cookieBanner").style.display = "block";
+    }
+}
